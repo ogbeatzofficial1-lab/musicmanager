dcf956c81050e349bf28385c2be74af8279b5cbb
ogbeatzofficial1-lab <ogbeatzofficial1@gmail.com>
2026-05-18 23:28:21 -0700

    Delete src directory

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * Utility for merging Tailwind classes with clsx logic.
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
